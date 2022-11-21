3b5ee915157881c25ad92e792dbd8d2ab22cc665
Mikko Tanhola <mikko.tanhola@metropolia.fi>
2022-11-21 14:17:38 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const PersonList = (props) => {
+  const {list} = props
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <ul style={{listStyle: "none"}}>
+        {list.map((person, i) => {return (
+          <li key={i}>{person.name} {person.number}</li>
+        )})}
+      </ul>
+    </>
+  )
 }
 
-export default App;
+const PersonForm = ({nameCh, numCh, submit, name, num}) => {
+  return(
+    <form onSubmit={submit}>
+        <div>
+          name: <input value={name} onChange={nameCh}/>
+        </div>
+        <div>
+          number: <input value={num} onChange={numCh}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+    </form>
+  )
+}
+
+const Filter = ({name, filterCh}) => {
+  return(
+    <>
+     filter shown with: <input onChange={filterCh} value={name}/> 
+    </>
+  )
+}
+
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNum, setNewNum] = useState('')
+  const [filter, setFilter] = useState('')
+ 
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const person = {
+      name: newName,
+      number: newNum
+    }
+
+    const filtered = persons.filter(person => person.name === newName).pop()
+
+    const checkAlert = () => {filtered ? alert(`${newName} is already added to phonebook`) : setPersons(persons.concat(person))}
+    checkAlert()
+    setNewName('')
+    setNewNum('')
+  }
+
+  const handleNameChange = ({target}) => setNewName(target.value)
+  const handleNumChange = ({target}) => setNewNum(target.value)
+  
+
+
+
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter name={filter} filterCh={({target}) => setFilter(target.value)}/>
+      <PersonForm 
+      nameCh={handleNameChange} 
+      numCh={handleNumChange} 
+      submit={handleSubmit}
+      name={newName}
+      num={newNum}
+      />
+      <h2>Numbers</h2>
+      <div>
+        <PersonList list={filter.length > 0 ? persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase())) : persons} />
+      </div>
+    </div>
+  )
+}
+
+export default App
